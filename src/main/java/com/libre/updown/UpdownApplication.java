3e174cbcd98face4c7da90fdab1c57cff6217c7f
ERi <erick_dr_17@hotmail.com>
2022-09-23 10:03:50 -0300

    Inicio de programa
    programa vacio, sin cambios

---
+++ src/main/java/com/libre/updown/UpdownApplication.java
@@ -0,0 +1,13 @@
+package com.libre.updown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libre/updown/UpdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libre.updown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
